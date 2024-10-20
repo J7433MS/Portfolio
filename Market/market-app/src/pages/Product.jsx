310833ad53bf2db9202b5a269fe1905687d04b42
J7433MS <j7433ms@gmail.com>
2024-10-20 17:19:44 +0900

    update

--- Market/market-app/src/pages/Product.jsx
+++ Market/market-app/src/pages/Product.jsx
@@ -1,4 +1,4 @@
-import { useCallback, useContext, useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom"
 import { ShopContext } from "../context/ShopContext";
 import { assets } from "../assets/market_assets/assets";
@@ -8,7 +8,7 @@ import RelatedProducts from "../components/RelatedProducts";
 const Product = () => {
 
   const { productId } = useParams();
-  const { products, currency } = useContext(ShopContext);
+  const { products, currency, addToCart } = useContext(ShopContext);
   const [productData, setProductData] = useState(false);
   const [image, setImage] = useState('');
   const [size, setSize] = useState('');
@@ -19,8 +19,6 @@ const Product = () => {
       if(item._id === productId){
         setProductData(item)
         setImage(item.image[0])
-        console.log(item);
-        
         return null;
       }
     })
@@ -67,11 +65,11 @@ const Product = () => {
           <p>Select Size</p>
           <div className="flex gap-2">
             {productData.sizes.map((item, index)=>(
-              <button onClick={()=>setSize(item)}className={`border py-2 px-4 bg-gray-100 ${item === size ? 'bg-orange-500' : ''}`} key={index}>{item}</button>
+              <button onClick={()=>setSize(item)} className={`border py-2 px-4 bg-gray-100 ${item === size ? 'bg-orange-500' : ''}`} key={index}>{item}</button>
             ))}
           </div>
         </div>
-        <button className="bg-black text-white px-8 py-3 text-sm active:bg-gray-700">ADD TO CART</button>
+        <button onClick={()=>addToCart(productData._id, size)} className="bg-black text-white px-8 py-3 text-sm active:bg-gray-700">ADD TO CART</button>
         <hr className="mt-8 sm:w-4/5" />
         <div className="text-sm text-gray-400 mt-5 flex flex-col gap-1">
           <p>100% Originall product.</p>
