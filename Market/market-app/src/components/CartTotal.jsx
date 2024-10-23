b3d7fca7ac409cd18864a8008559702bcd850649
J7433MS <j7433ms@gmail.com>
2024-10-23 18:06:18 +0900

    Update

--- Market/market-app/src/components/CartTotal.jsx
+++ Market/market-app/src/components/CartTotal.jsx
@@ -1,8 +1,33 @@
+import { useContext } from "react"
+import { ShopContext } from "../context/ShopContext"
+import Title from './Title';
 
 const CartTotal = () => {
+
+  const { currency, delivery_fee, getCartAmount } = useContext(ShopContext);
+
   return (
-    <div>
-      
+    <div className="w-full">
+      <div className="text-2xl">
+        <Title text1={'CART'} text2={'TOTALS'} />
+      </div>
+
+      <div className="flex flex-col gap-2 mt-2 text-sm">
+        <div className="flex justify-between">
+          <p>Subtotal</p>
+          <p>{currency} {getCartAmount()}.00</p>
+        </div>
+        <hr />
+        <div className="flex justify-between">
+          <p>Shipping Fee</p>
+          <p>{currency} {delivery_fee}</p>
+        <hr />
+        <div className="flex justify-between">
+          <p>Total</p>
+          <p>{currency} {getCartAmount() === 0 ? 0 : getCartAmount() + delivery_fee}</p>
+        </div>
+        </div>
+      </div>
     </div>
   )
 }
